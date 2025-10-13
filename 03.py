fee50f87a2e7ddef474ebf0b2521b31fe11e46dd
linhphambuzz <mylinh.phamtran@gmail.com>
2025-10-13 17:47:27 -0500

    tốt gỗ hơn tốt nước sơn

---
+++ 03.py
@@ -0,0 +1,11 @@
+""" #27. Remove Element - Easy  """
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        idx=0
+        for num in nums:
+            if num!=val:
+                nums[idx]=num
+                idx+=1
+        
+        return idx
